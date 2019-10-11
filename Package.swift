271a3b32d268605a222615d3d8ec8f88ad5cb831
Lennart Fischer <lennart.fischer@adolfinum.de>
2019-10-11 17:43:12 +0200

    Fixed Platform Version

--- Package.swift
+++ Package.swift
@@ -1,11 +1,10 @@
 // swift-tools-version:5.1
-
 import PackageDescription
 
 let package = Package(
   name: "Fuse",
   platforms: [
-    .iOS(.v12)
+    .iOS(.v8)
   ],
   products: [
     .library(name: "Fuse", targets: ["Fuse"]),
